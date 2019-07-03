d79dcb0837730d30a73a3582946968668892ba2f
Curso Universidad UNITEC Honduras01 <unitec01@EVIRL-005-OK.dsic.cloud>
2019-07-03 12:46:58 +0200

    Creacion de los primeros atributos del agente inteligente

--- src/agentenegociador/Agentes/ArlesAgentes.java
+++ src/agentenegociador/Agentes/ArlesAgentes.java
@@ -5,10 +5,37 @@
  */
 package agentenegociador.Agentes;
 
+import negoUPV.UPVAgent;
+import negotiator.Bid;
+
 /**
  *
  * @author unitec01
  */
-public class ArlesAgentes {
+public class ArlesAgentes extends UPVAgent{
+    
+    Bid mi_ultima_oferta;
+    double S;
+    double beta;
+    double RU;
+    
+
+    @Override
+    public void initialize() {
+        mi_ultima_oferta = null;
+        beta = 0.50;
+        RU =  0.75;
+        S = 0.70;
+    }
+
+    @Override
+    public Bid proposeOffer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean acceptOffer(Bid bid) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
